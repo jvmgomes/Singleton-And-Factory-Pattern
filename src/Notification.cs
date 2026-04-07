c23a48023cc4664e1e90056bbb501e014de68324
jvmgomes <joao@mgomes.net>
2026-04-07 17:25:14 -0300

    Create Notification.cs

---
+++ src/Notification.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Padrões_de_Projeto___a
+{
+    public interface Notification
+    {
+        void send(string message);
+    }
+}
